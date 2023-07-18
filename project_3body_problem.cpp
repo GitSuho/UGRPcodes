6cae3ec3a6072d4785777fd3b85d6a587a9dcd3a
GitSuho <cjsuhod0518@dgist.ac.kr>
2023-07-18 13:21:50 +0900

    v1.1.1

--- project_3body_problem.cpp
+++ project_3body_problem.cpp
@@ -6,6 +6,13 @@ using namespace std;
 int Norm(double _x, double _y){
     return sqrt(_x*_x+_y*_y);
 }
+string write_contents(long double _X[][4]){
+    string result = "|";
+    for(int i = 0 ; i < 3; i ++){for (int j = 0; j < 4; j++){
+        result += to_string(_X[i][j]) + "|"; 
+    }}
+    return result;
+}
 
 double m_val[3] = {1, 1, 1};
 double G = 9.8; double dt = 0.01; 
@@ -22,14 +29,18 @@ long double E = (0.5)*m_val[0]*(initial_val[0][2]*initial_val[0][2] + initial_va
 
 long double T( long double _xv_array[][4]){
     long double T = E;
+cout << E << endl;
     for (int i = 0 ; i < 3 ; i++){
         int j = (i+1)%3;
         T += G*m_val[i]*m_val[j]/Norm(_xv_array[i][0]-_xv_array[j][0], 
                                       _xv_array[i][1]-_xv_array[j][1]);
     } 
+// cout << T << endl;
     return T;
 }
 long double T_diff(long double _xv_array[][4]){
+// cout << write_contents(_xv_array) << endl;
+
     long double T_diff = 0;
     for (int i = 0; i < 3; i++){
         int j = (i+1)%3;
@@ -39,6 +50,7 @@ long double T_diff(long double _xv_array[][4]){
         /((_xv_array[i][0]-_xv_array[j][0])*(_xv_array[i][0]-_xv_array[j][0])
          +(_xv_array[i][1]-_xv_array[j][1])*(_xv_array[i][1]-_xv_array[j][1]));
     }
+// cout << T_diff << endl;
     return T_diff;
 }
 long double diff_T_xv(int i, int j, long double _xv_array[][4]){
@@ -50,19 +62,15 @@ long double diff_T_xv(int i, int j, long double _xv_array[][4]){
               +(_xv_array[i][1]-_xv_array[j][1])*(_xv_array[i][1]-_xv_array[j][1]))
              *((_xv_array[i][0]-_xv_array[j][0])*(_xv_array[i][0]-_xv_array[j][0])
               +(_xv_array[i][1]-_xv_array[j][1])*(_xv_array[i][1]-_xv_array[j][1])));
-cout << sqrt((_xv_array[i][0]-_xv_array[j][0])*(_xv_array[i][0]-_xv_array[j][0])
-              +(_xv_array[i][1]-_xv_array[j][1])*(_xv_array[i][1]-_xv_array[j][1]))
-             *((_xv_array[i][0]-_xv_array[j][0])*(_xv_array[i][0]-_xv_array[j][0])
-              +(_xv_array[i][1]-_xv_array[j][1])*(_xv_array[i][1]-_xv_array[j][1])) <<endl;
-
-
     }
 // cout << result << endl;
     return result;
 }
 void F( long double _xv_array[][4], long double F[][4]){
+
     long double dT;
     for (int i = 0; i < 3 ; i++){for( int j = 0 ; j < 2 ; j ++ ){
+// cout << T(_xv_array) << endl;
         F[i][j] = T_diff(_xv_array)/T(_xv_array)*_xv_array[i][j+2];
         for(int k = 0 ; k < 3; k++){for(int l = 0; l < 2; l++){
             dT = diff_T_xv(i, j, _xv_array);
@@ -70,6 +78,7 @@ void F( long double _xv_array[][4], long double F[][4]){
             F[i][j] += - 1/T(_xv_array)*dT*_xv_array[k][l+2]
                         + 1/(2*T(_xv_array))*dT*(_xv_array[k][l+2]*_xv_array[k][l+2]);
         }}}}
+// cout << write_contents(F) << endl;
 }
 void geod( long double _X[][4], long double result_ptr[][4]){
     long double F1[3][4]; long double F2[3][4]; 
@@ -90,13 +99,7 @@ void sum_two_ptr(long double ptr1[][4], long double ptr2[][4]){
         ptr2[i][j] += ptr1[i][j];
     }}
 }
-string write_contents(long double _X[][4]){
-    string result = "|";
-    for(int i = 0 ; i < 3; i ++){for (int j = 0; j < 4; j++){
-        result += to_string(_X[i][j]) + "|"; 
-    }}
-    return result;
-}
+
 
 int main(){
     string filename = "123";
@@ -111,7 +114,7 @@ int main(){
 
     long double k1[3][4];long double k2[3][4];long double k3[3][4];long double k4[3][4];
     long double extra_k[3][4];
-    for (double t = 0.0; t < 5; t += dt){
+    for (long double t : {0.0, 0.001}){ //= 0.0; t < 5; t += dt){
         geod(X, k1);
         const_multiply_array(dt/2, k1, extra_k);sum_two_ptr(X, extra_k);geod(extra_k, k2);
         const_multiply_array(dt/2, k2, extra_k);sum_two_ptr(X, extra_k);geod(extra_k, k3);        
@@ -122,7 +125,7 @@ int main(){
                 X[i][j] = X[i][j] + (dt / 6) * (k1[i][j] + 2 * k2[i][j] + 3 * k3[i][j] + 4 * k4[i][j]);
 // cout << k1[i][j] << endl;
             }}
-
+cout << "-----------------------" << endl;
         writefile << write_contents(X) << endl;
     }
     writefile.close(); cout << "program end!!!"<<endl;
