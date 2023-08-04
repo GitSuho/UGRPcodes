e7577c19596098f989c9e646109c3f0ade1b8954
GitSuho <cjsuhod0518@dgist.ac.kr>
2023-08-04 14:06:48 +0900

    v2.0.2

--- ThreeBodyTeam.m
+++ ThreeBodyTeam.m
@@ -1,6 +1,6 @@
-x_init = [0 2; 1.5 -1; -1.5 -1]; % [x1 x2; x3 x4; x5 x6] init position
-v_init = [-sqrt(3.25) 0; 1.5 -1; 1.5, 1]; % [x1' x2'; x3' x4'; x5' x6'] init velocity
-m = [1 1.5 2]; % masses
+x_init = [-1/2 -1/3; 1/2 -1/3; 0 2/3]; 
+v_init = [1/3 -2/3; -1/3 2/3; -1 0]; 
+m = [1 1 1];
 G = 9.8;
 init = [x_init v_init]';
 init = init(:)';
@@ -13,10 +13,6 @@ for i = 0:2
     E = E + 1/2*m(j)*norm(v_init(j,:))^2 ...
         - G*m(j)*m(k)/norm(x_init(j,:)-x_init(k,:));
 end
-
-% [x1  x2  x1' x2']
-% [x3  x4  x3' x4']
-% [x5  x6  x5' x6']
 
 syms('x_sym', [3 4]);
 assume(x_sym, 'real')
@@ -37,7 +33,6 @@ for i = 0:2
                                 / norm(x_sym(j,1:2)-x_sym(k,1:2))^2 ;
 end
 
-
 global F;
 F{3,2} = [];
 f{3,2} = [];
@@ -54,20 +49,28 @@ for i = 1:3
     end
 end
 
-terminal_p = 20;
-div_N = 100;
-t = linspace(0, terminal_p, terminal_p*div_N);
-[t, x] = RK4(@g, t, init); % Use @g to pass g as a function handle
-
-% plot
-plot(x(:,1),x(:,2),'linewidth',2);
+start = 0;
+terminal = 10;
+div_N = 50;
+count_div = 10;
+plot(init(1), init(2));
 hold on;
-plot(x(:,5),x(:,6),'linewidth',2);
-plot(x(:,9),x(:,10),'linewidth',2);
+for count = 1:count_div
+    fprintf("count num : %d\n" , count);
+    start_p = start / count_div * count;
+    terminal_p = terminal / count_div    * count;
+
+    t = linspace(start_p, terminal_p, terminal_p*div_N);
+    [t, x] = RK4(@g, t, init); 
+    init = x(length(x),:);
+    plot(x(:,1),x(:,2),'linewidth',2);
+    plot(x(:,5),x(:,6),'linewidth',2);
+    plot(x(:,9),x(:,10),'linewidth',2);
+end
 hold off;
 
-function dxdt = g(t, x) % Change the order of input arguments
-    global F; % Declare F as a global variable
+function dxdt = g(t, x) 
+    global F; 
     dxdt = zeros(size(x));
     for i = 1:3
         for j = 1:2
@@ -77,19 +80,17 @@ function dxdt = g(t, x) % Change the order of input arguments
     end
 end
 
-% ... (Rest of the code)
-
-% Modify the RK4 function to handle vectors and return both t and X
 function [t, X] = RK4(func, t, prior_x)
     dt = t(2) - t(1);
     nt = length(t);
     X = zeros([nt, numel(prior_x)]);
     X(1,:) = prior_x;
     for i = 1:nt-1
-        k1 = func(t(i), X(i,:)); % Pass t and X as separate arguments
-        k2 = func(t(i) + dt/2, X(i,:) + k1*dt/2); % Pass t and X as separate arguments
-        k3 = func(t(i) + dt/2, X(i,:) + k2*dt/2); % Pass t and X as separate arguments
-        k4 = func(t(i) + dt, X(i,:) + k3*dt); % Pass t and X as separate arguments
+        k1 = func(t(i), X(i,:)); 
+        k2 = func(t(i) + dt/2, X(i,:) + k1*dt/2); 
+        k3 = func(t(i) + dt/2, X(i,:) + k2*dt/2); 
+        k4 = func(t(i) + dt, X(i,:) + k3*dt); 
         X(i+1,:) = X(i,:) + (k1 + 2*k2 + 2*k3 + k4)*dt/6;
     end
 end
+
