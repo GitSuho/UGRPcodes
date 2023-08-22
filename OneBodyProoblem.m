5fe24d694af07699f4e6d7a9dc313c4532313868
GitSuho <cjsuhod0518@dgist.ac.kr>
2023-08-22 10:21:23 +0900

    v2.3.5

--- OneBodyProoblem.m
+++ OneBodyProoblem.m
@@ -2,9 +2,9 @@
 
 
 %Initial consitions
-x_init = [1, 0];
-v_init = [0, 1];
-M = 3;m = 5 ;G = 1;
+x_init = [3, 0];
+v_init = [0, 2];
+M = 7;m = 1 ;G = 1;
 
 syms('x_sym', [2, 2]);
 assume(x_sym, 'real');
@@ -29,7 +29,7 @@ global GE_t;
 GE_t{2} = [];
 ge_t{2} = [];
 for i = 1:2
-    ge_t{i} = (G*M*m*dot(x_sym(1, :), x_sym(2, :))/(norm(x_sym(1, :))^2))/T*x_sym(2, i) ...
+    ge_t{i} = (2*G*M*m*dot(x_sym(1,:), x_sym(2,:))/((x_sym(1,1)^2+x_sym(1,2)^2)^2))/T*x_sym(2, i) ...
     -(1/T)*diff(T, x_sym(1, mod(i, 2)+1))*x_sym(2, mod(i, 2)+1)*x_sym(2, i) ... 
     + 1/(2*T)*diff(T, x_sym(1, i))*(x_sym(2, mod(i, 2)+1)^2);
     GE_t{i} = @(x) eval(subs(ge_t{i}, x_sym, reshape(x,[2,2])'));
@@ -64,6 +64,7 @@ for i = 1:count
     orbit_coordinate(i, :) = OrbEqu(theta_arr(i));
 end
 plot(orbit_coordinate(:, 1), orbit_coordinate(:, 2), 'black-');
+plot(0, 0, 'blacko');
 
 
 dt = 1/100;
@@ -76,7 +77,6 @@ X_t(1, :) = init;
 P(1, :) = init;
 
 % %plot numerical solutions
-% plot(0, 0, 'blacko');
 % while(1)
 %     for i = 1:cycle
 %         X(i+1, :) = RK4(X(i, :), @geo, dt);
@@ -93,29 +93,52 @@ P(1, :) = init;
 %     pause(0.1);
 % end
 
-% %calculate error values
-% col_num = 100;
-% clear("X_error")
-% clear("X_t_error")
-% clear("P_error")
-% X_error(1, :) = [0, 0];
-% X_t_error(1, :) = [0, 0];
-% P_error(1, :) = [0, 0];
-% for i = 1:col_num-1
-%         X(i+1, :) = RK4(X(i, :), @geo, dt);
-%         X_t(i+1, :) = RK4(X_t(i, :), @geo_t, dt);
-%         P(i+1, :) = RK4(P(i, :), @lag, dt);
-% end
-% for i = 1:col_num
-%     foo_theta = atan2(X(i,1), X(i,2));
+%calculate error values
+X_count = 170;
+X_t_count = 1;
+P_count = 1000;
+
+X_dt = 1/10; 
+X_t_dt = 1/100; 
+P_dt = 1/10;
+
+clear("X_error")
+clear("X_t_error")
+clear("P_error")
+X_error(1, :) = [0, 0];
+X_t_error(1, :) = [0, 0];
+P_error(1, :) = [0, 0];
+
+for i = 1:X_count-1
+    X(i+1, :) = RK4(X(i, :), @geo, X_dt);
+    % foo = norm(X(i+1, 3:4));
+    % X(i+1, 3:4) = X(i+1, 3:4)/foo;
+end
+for i = 1:X_t_count-1
+    X_t(i+1, :) = RK4(X_t(i, :), @geo_t, X_t_dt);
+end
+for i = 1:P_count-1
+    P(i+1, :) = RK4(P(i, :), @lag, P_dt);
+    % foo = norm(P(i+1, 3:4));
+    % P(i+1, 3:4) = P(i+1, 3:4)/foo;
+end
+
+plot(X(:, 1), X(:, 2), 'bo');
+plot(X_t(:, 1), X_t(:, 2), 'gp');
+plot(P(:, 1), P(:, 2), 'r^');
+% 
+% for i = 1:X_count
+%     foo_theta = Find_degree(X(i,1), X(i,2));
 %     var_error = abs(OrbEqu_r(foo_theta) - norm(X(i, 1:2)));
 %     X_error(i, :) = [foo_theta var_error];
-% 
-%     foo_theta = atan2(X_t(i,1), X_t(i,2));
+% end
+% for i = 1:X_t_count
+%     foo_theta = Find_degree(X_t(i,1), X_t(i,2));
 %     var_error = abs(OrbEqu_r(foo_theta) - norm(X_t(i, 1:2)));
 %     X_t_error(i, :) = [foo_theta var_error];
-% 
-%     foo_theta = atan2(P(i,1), P(i,2));
+% end
+% for i = 1:P_count
+%     foo_theta = Find_degree(P(i, 1), P(i, 2));
 %     var_error = abs(OrbEqu_r(foo_theta) - norm(P(i, 1:2)));
 %     P_error(i, :) = [foo_theta var_error];
 % end
@@ -174,7 +197,13 @@ function r = OrbEqu_r(theta)
     r = 1/(A_coeff*cos(theta)+(k_const/(l_const.^2)));
 end
 
-
+function degree = Find_degree(x, y)
+    if (y >= 0 )
+        degree = acos( x/sqrt(x.^2 + y.^2));
+    else
+        degree = 2*pi - acos( x/sqrt(x.^2 + y.^2));
+    end
+end
 
 
 
